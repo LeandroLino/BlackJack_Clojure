4314e5bcd602c97c1e36aa555ceedc599184cdfb
Leandro Lino <leao.lino7@gmail.com>
2022-03-16 13:02:06 -0300

    BJ-Clojure :: update :: Criaçao de player

--- src/black_jack/game.clj
+++ src/black_jack/game.clj
@@ -4,5 +4,11 @@
   "Generates a card number between 1 and 13"
   (inc (rand-int 13)))
 
+(defn player [player-name]
+  (def card-1 (new-card))
+  (def card-2 (new-card))
+  {:player player-name
+   :cards [card-1 card-2]})
+
 (defn -main []
   "Defn main")
